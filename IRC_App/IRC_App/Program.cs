ea9160a2e405b1fafd42a08dd59f80aa38d61ce6
theTwister <theTwist84@users.noreply.github.com>
2016-03-22 22:18:32 +0000

    Additions and changes
    Added:
    - Colored output for certain events.
    
    Changes:
    - Removed 'string Date = DateTime.Now.ToString("hh:mm:ss tt");' from all events, 
    and Moved 'string Date = DateTime.Now.ToLongTimeString();' at the beginning of the class.

--- IRC_App/IRC_App/Program.cs
+++ IRC_App/IRC_App/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Text.RegularExpressions;
 
@@ -25,8 +25,8 @@ namespace IRC_App
         public static void Log(string Message)
         {
             string Dir = Directory.GetCurrentDirectory();
-            string Date = DateTime.Today.Year + "-" + DateTime.Today.Month + "-" + DateTime.Today.Day;
-            string Time = DateTime.Now.ToString("hh:mm:ss tt");
+            string Date = DateTime.Today.ToShortDateString();
+            string Time = DateTime.Now.ToLongTimeString();
 
             string L_Log = String.Format("{0}\\Log_{1}.txt", Dir, Date);
             string L_Url = String.Format("{0}\\Url_{1}.txt", Dir, Date);
@@ -70,9 +70,16 @@ namespace IRC_Lib
         
         public static void ChannelRecieved(IrcClient client, IRC_App.Properties.Settings vars)
         {
+            string Date = DateTime.Now.ToLongTimeString();
+
             client.ChannelTopicReceived += (s, e) =>
             {
                 string Topic = e.Channel.Topic;
+                
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("{0}", Topic);
+                Console.ResetColor();
 
                 if (!(Topic.Contains("ftp://") || Topic.Contains("http://") || Topic.Contains("https://")))
                 {
@@ -85,16 +92,29 @@ namespace IRC_Lib
 
             client.ChannelMessageRecieved += (s, e) =>
             {
-                string Date = DateTime.Now.ToString("hh:mm:ss tt");
                 string Nick = Regex.Split(e.PrivateMessage.User.Nick, "!")[0];
                 string Message = e.PrivateMessage.Message;
 
                 var channel = client.Channels[e.PrivateMessage.Source];
 
-                Console.WriteLine("[{0}] {1}: {2}", 
-                    Date, Nick, Message);
-                Logger.Log(String.Format("[{0}] {1}: {2}", 
-                    Date, Nick, Message));
+                switch (Message.Contains(vars.IRC_Nick) || Message.Contains(vars.IRC_User))
+                {
+                    case true:
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("[{0}] {1}: {2}",
+                            Date, Nick, Message);
+                        Console.ResetColor();
+                        Logger.Log(String.Format("[{0}] {1}: {2}",
+                            Date, Nick, Message));
+                        break;
+                    case false:
+                        Console.WriteLine("[{0}] {1}: {2}",
+                            Date, Nick, Message);
+                        Logger.Log(String.Format("[{0}] {1}: {2}",
+                            Date, Nick, Message));
+                        break;
+                }
 
                 switch (Message)
                 {
@@ -131,54 +151,87 @@ namespace IRC_Lib
 
         public static void UserRecieved(IrcClient client, IRC_App.Properties.Settings vars)
         {
+            string Date = DateTime.Now.ToLongTimeString();
+
             client.UserJoinedChannel += (s, e) =>
             {
-                string Date = DateTime.Now.ToString("hh:mm:ss tt");
                 string Nick = Regex.Split(e.User.Nick, "!")[0];
                 string Host = Regex.Split(e.User.Hostmask, "!")[1];
-                string Channel = e.Channel.Name;
+                var Channel = e.Channel;
 
                 switch (Nick == vars.IRC_Nick)
                 {
                     case true:
-                        Console.WriteLine("[{0}] Now talking on {1}", 
-                            Date, Channel);
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("[{0}] Now talking on {1}",
+                            Date, Channel.Name);
+                        Console.ResetColor();
                         Logger.Log(String.Format("[{0}] Now talking on {1}", 
-                            Date, Channel));
+                            Date, Channel.Name));
                         break;
                     case false:
-                        Console.WriteLine("[{0}] {1} ({2}) has joined", 
+                        Console.BackgroundColor = ConsoleColor.DarkGreen;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("[{0}] {1} ({2}) has joined",
                             Date, Nick, Host);
+                        Console.ResetColor();
                         Logger.Log(String.Format("[{0}] {1} ({2}) has joined", 
                             Date, Nick, Host));
                         break;
                 }
-                switch (Nick)
+
+                switch (Nick.ToLower())
                 {
-                    case "theTwister":
-                        e.Channel.SendMessage("Thank you based programmer for my existence!");
+                    case "thetwister":
+                        Channel.SendMessage("Thank you based programmer for my existence!");
+                        Console.WriteLine("Sent welcome message to {0}", Nick);
                         break;
-                    case "Shockfire":
-                        e.Channel.SendMessage("theTwister made me a Synth!");
+                    case "shockfire":
+                        Channel.SendMessage("theTwister made me a Synth!");
+                        Console.WriteLine("Sent welcome message to {0}", Nick);
                         break;
                     case "dany":
-                        e.Channel.SendMessage("Sup dany... Hows the current progress on [insert latest project here]!");
+                        Channel.SendMessage(String.Format("Sup {0}... Hows the current progress on [insert latest project here]!", Nick));
+                        Console.WriteLine("Sent welcome message to {0}", Nick);
                         break;
-                    case "Camden":
-                        e.Channel.SendMessage("Ayy!");
+                    case "camden":
+                        Channel.SendMessage("Ayy!");
+                        Console.WriteLine("Sent welcome message to {0}", Nick);
                         break;
-                    case "DEElekgolo":
-                        e.Channel.SendMessage("A wild DEElekgolo has appeared!");
+                    case "deelekgolo":
+                        Channel.SendMessage("A wild DEElekgolo appeared!");
+                        Console.WriteLine("Sent welcome message to {0}", Nick);
                         break;
                     case "emoose":
-                        e.Channel.SendMessage("Our lord and savior has returned!");
+                        Channel.SendMessage("Our lord and savior has returned!");
+                        Console.WriteLine("Sent welcome message to {0}", Nick);
                         break;
+                    case "alex231":
+                        Channel.SendMessage("HUD master!");
+                        Console.WriteLine("Sent welcome message to {0}", Nick);
+                        break;
+                    case "rabidsquabbit":
+                        Channel.SendMessage("Host master!");
+                        Console.WriteLine("Sent welcome message to {0}", Nick);
+                        break;
+                    case "divide":
+                    case "g-money":
+                    case "weion":
+                        Channel.SendMessage("Host disciple!");
+                        Console.WriteLine("Sent welcome message to {0}", Nick);
+                        break;
+                }
+
+                if (Nick.ToLower().StartsWith("snoo") || Nick.ToLower().StartsWith("eldorito"))
+                {
+                    Channel.SendMessage(String.Format("Welcome {0}, What is your enquiry", Nick));
+                    Console.WriteLine("Sent welcome message to {0}", Nick);
                 }
             };
 
             client.UserKicked += (s, e) =>
             {
-                string Date = DateTime.Now.ToString("hh:mm:ss tt");
                 string Kicker = Regex.Split(e.Kicker.Nick, "!")[0];
                 string Kicked = Regex.Split(e.Kicked.Nick, "!")[0];
                 string Channel = e.Channel.Name;
@@ -187,14 +240,20 @@ namespace IRC_Lib
                 switch (Reason == null)
                 {
                     case true:
-                        Console.WriteLine("[{0}] {1} has kicked {2} from {3}", 
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("[{0}] {1} has kicked {2} from {3}",
                             Date, Kicker, Kicked, Channel);
+                        Console.ResetColor();
                         Logger.Log(String.Format("[{0}] {1} has kicked {2} from {3}", 
                             Date, Kicker, Kicked, Channel));
                         break;
                     case false:
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("[{0}] {1} has kicked {2} from {3} (reason {4})",
                             Date, Kicker, Kicked, Channel, Reason);
+                        Console.ResetColor();
                         Logger.Log(String.Format("[{0}] {1} has kicked {2} from {3} (reason {4})", 
                             Date, Kicker, Kicked, Channel, Reason));
                         break;
@@ -203,22 +262,26 @@ namespace IRC_Lib
 
             client.UserPartedChannel += (s, e) =>
             {
-                string Date = DateTime.Now.ToString("hh:mm:ss tt");
                 string Nick = Regex.Split(e.User.Nick, "!")[0];
 
+                Console.BackgroundColor = ConsoleColor.DarkGray;
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("[{0}] {1} has left",
                     Date, Nick);
+                Console.ResetColor();
                 Logger.Log(String.Format("[{0}] {1} has left",
                     Date, Nick));
             };
 
             client.UserQuit += (s, e) =>
             {
-                string Date = DateTime.Now.ToString("hh:mm:ss tt");
                 string Nick = Regex.Split(e.User.Nick, "!")[0];
 
+                Console.BackgroundColor = ConsoleColor.DarkGray;
+                Console.ForegroundColor = ConsoleColor.Black;
                 Console.WriteLine("[{0}] {1} has quit",
                     Date, Nick);
+                Console.ResetColor();
                 Logger.Log(String.Format("[{0}] {1} has quit",
                     Date, Nick));
             };
